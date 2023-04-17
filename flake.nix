80aa32063f0a705ca7cf3fc4ffcec42e4c208345
Jamie Davies <jamie@jagd.me>
2023-04-17 09:02:46 +0100

    Build custom neovim package using rose-pine colorscheme plugin

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       url = github:nix-community/neovim-nightly-overlay;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    rose-pine = {
+      url = github:rose-pine/neovim;
+      flake = false;
+    };
   };
 
   outputs = {
@@ -18,18 +23,67 @@
     ...
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
+      plugins = [
+        "rose-pine"
+      ];
+
+      pluginOverlay = final: prev: let
+        buildPlug = name:
+          prev.vimUtils.buildVimPluginFrom2Nix {
+            pname = name;
+            version = "master";
+            src = builtins.getAttr name inputs;
+          };
+      in {
+        neovimPlugins =
+          builtins.listToAttrs
+          (map (name: {
+              inherit name;
+              value = buildPlug name;
+            })
+            plugins);
+      };
+
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [inputs.neovim-nightly-overlay.overlay];
+        overlays = [
+          inputs.neovim-nightly-overlay.overlay
+          pluginOverlay
+        ];
       };
-    in {
+
+      neovimBuilder = {
+        customRC ? "",
+        viAlias ? true,
+        vimAlias ? true,
+        start ? builtins.attrValues pkgs.neovimPlugins,
+        opt ? [],
+      }:
+        pkgs.wrapNeovim pkgs.neovim-nightly {
+          configure = {
+            inherit customRC viAlias vimAlias;
+
+            packages.jagd = {
+              inherit start opt;
+            };
+          };
+        };
+    in rec {
       apps.default = {
         type = "app";
-        program = "${pkgs.neovim-nightly}/bin/nvim";
+        program = "${packages.default}/bin/nvim";
       };
 
       formatter = pkgs.alejandra;
 
-      packages.default = pkgs.neovim-nightly;
+      packages = rec {
+        default = neovimJagd;
+        neovimJagd = neovimBuilder {
+          customRC = ''
+            colorscheme rose-pine
+            echom "Hello from jagd"
+          '';
+        };
+      };
     });
 }
